47f57d949d15b5dc3b0d042c913b9df378abbb00
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2024-10-23 18:06:53 +0000

    naerly htere

--- BNC.py
+++ BNC.py
@@ -1,7 +1,19 @@
-def binaryToDecimal(num):
-    num = input("pgive a number from 1-99 ")
-    num2 = "14"
-    if num >= num2:
-        num - num2
-print (binaryToDecimal)
-binaryToDecimal(14)
+# def binaryToDecimal(num):
+#     num = input("give a number from 0-15 ")
+#     int(num // 2)
+# print (binaryToDecimal)
+# binaryToDecimal(14)
+
+# bin(7)
+# print(bin)
+
+num = input("give a number from 0-15 ")
+if num == "1":
+    num1 = 1
+binary_converter = bin(num1)
+if num == "2":
+    num2 = 2
+binary_converter = bin(num2)
+print(binary_converter)
+
+
