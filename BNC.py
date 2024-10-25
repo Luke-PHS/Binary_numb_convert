0e62e09781e2e16605eee9befeb0963c8955aaf6
Luke-PHS <144828720+Luke-PHS@users.noreply.github.com>
2024-10-25 19:00:53 +0000

    I DID IT

--- BNC.py
+++ BNC.py
@@ -7,13 +7,51 @@
 # bin(7)
 # print(bin)
 
-num = input("give a number from 0-15 ")
+binary_converter = num = input("give a number from 0-15 ")
 if num == "1":
-    num == 1
-binary_converter = bin(num)
-if num == "2":
-    num = 2
-binary_converter = bin(num)
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "2":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "3":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "4":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "5":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "6":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "7":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "8":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "9":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "10":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "11":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "12":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "13":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "14":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+elif num == "15":
+    number_int = int(num)
+    binary_converter = bin(number_int)
+
 print(binary_converter)
-
-
